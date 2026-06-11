3ba0383a32b839b4391a72526754a72ead736a86
Antony <antony.programmer@gmail.com>
2026-06-10 20:06:04 -0400

    feat: Se definio la funcion gastar_todo()

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,47 +1,32 @@
 # ejercicio de simulacion del hijo prodigo
-
-# variables
-
-nombre = input("Ingrese su nombre: ")
-dinero = 100
-dignidad = 50
-hambre = 0
-
-print("Ustede a recibido su herencia.")
-
-# condiciones
-# mostrar 3 opciones: Gastarlo todo, Invertir una parte, Ahorrar
-print("Seleccione una opcion\n")
-
-print("1. Gastarlo todo en fiestas")
-print("3. Invertir una parte")
-print("3. Ahorrar")
-opcion = input("Opcion: ")
-if opcion == 1:
-    dinero = 0
-    dignidad -= 20
-    hambre += 50
-elif opcion == 2:
-    dinero +=20
-elif opcion ==3:
-    print("Usted esta ahorrando")
-else:
-    print("Opcion invalida")
-
-def gastar(dinero, dignidad):
-    pass
-
-def trabajar(dinero, hambre):
-    pass
-
-while dinero <= 0:
-    print("Sigues viviendo en casa")
-    dinero -= 10
-
 class HijoProdigo:
-   def __init__(self, nombre,  dignidad, hambre, arrepentimiento):
-       self.nombre = nombre
-       self.dinero = 100
-       self.dignidad =50
-       self.hambre = 0
-       self.arrepentimiento = 0
+    def __init__(self, nombre, arrepentimiento = 0, dinero=100, dignidad=50, hambre=0):
+        self.nombre = nombre
+        self.dinero = dinero
+        self.dignidad = dignidad
+        self.hambre = hambre
+        self.arrpentimiento = arrepentimiento
+
+    def gastar_todo(self, dinero, dignidad, hambre):
+        # el problema no da indicaciones exactas de como realizar, asi que lo hare como se pueda
+        if dinero > 0:
+            print("Estas de fiesta y gastando tu dinero")
+            dinero -= 20 #Cada fiesta reducira 20 monedas
+            dignidad -= 20
+            hambre += 50
+            print(f"Estado: \nDinero = {dinero}\nDignidad = {dignidad}\nHambre = {hambre}")
+
+    def invertir(self):
+        pass
+    def ahorrar(self):
+        pass
+    def trabajar(self):
+        pass
+    def reflexionar(self, arrepentimiento, hambre):
+        if hambre > 40:
+            arrepentimiento+=10
+
+
+print("Bienvenido al juego del hijo prodigo")
+jugador = HijoProdigo(input("Ingrese su nombre: "))
+jugador.gastar_todo(jugador.dinero,jugador.dignidad,jugador.hambre)
