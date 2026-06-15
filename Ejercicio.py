135b793229bf5619a7fa7d13becc9a67d22a5349
Antony <antony.programmer@gmail.com>
2026-06-14 20:37:27 -0400

    fix: se arreglo el metodo gastar_todo, y se coloco opciones

--- Ejercicio.py
+++ Ejercicio.py
@@ -8,7 +8,7 @@ class HijoProdigo:
         self.arrpentimiento = arrepentimiento
         print(f"\nBienvenido {nombre}")
         print(f"Felicidades, recibiste tu herrencia de {dinero}")
-        print("Que quieres hacer?")
+
 
     def gastar_todo(self, dinero, dignidad, hambre):
         # el problema no da indicaciones exactas de como realizar, asi que lo hare como se pueda
@@ -17,12 +17,16 @@ class HijoProdigo:
         decicion = int(input("1: SI\n2: NO\n:"))
         if decicion == 1:
             print("Te fuiste de casa")
-        if dinero > 0:
-            print("Estas de fiesta y gastando tu dinero")
-            dinero -= 20 #Cada fiesta reducira 20 monedas
-            dignidad -= 20
-            hambre += 50
-            print(f"Estado: \nDinero = {dinero}\nDignidad = {dignidad}\nHambre = {hambre}")
+            if dinero > 0:
+                print("Estas de fiesta y gastando tu dinero")
+                dinero -= 20 #Cada fiesta reducira 20 monedas
+                dignidad -= 20
+                hambre += 50
+                print(f"Estado: \nDinero = {dinero}\nDignidad = {dignidad}\nHambre = {hambre}")
+        elif decicion == 2:
+            return
+        else:
+            print("Opcion incorrecta")
 
     def invertir(self, dinero):
         print("Usted esta invirtiendo")
@@ -48,6 +52,18 @@ class HijoProdigo:
 
 print("Bienvenido al juego del hijo prodigo")
 jugador = HijoProdigo(input("Ingrese su nombre: "))
+intentos = 0
+opcion = 0
+while jugador.dinero >= 0:
 
-while jugador.dinero <= 0:
-    pass
+    if intentos == 0:
+        print("Que quieres hacer?\n")
+        print("1: Gastarlo todo en fiestas")
+        print("2: Invertir una parte")
+        print("3: Ahorrar")
+        print("4: Trabajar")
+        #intentos += 1
+        opcion = int(input("\n Opcion: "))
+        if opcion == 1:
+            jugador.gastar_todo(jugador.dinero, jugador.dignidad, jugador.hambre)
+
